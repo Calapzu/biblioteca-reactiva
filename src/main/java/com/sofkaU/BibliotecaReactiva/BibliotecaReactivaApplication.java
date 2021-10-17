f509e57d51df67f4c7dff158f7870017eb849b2f
Calapzu <yamsoncalapzu@gmail.com>
2021-10-17 00:40:18 -0500

    Se empieza a cosntruir el proyecto con las carpertas correpondientes

---
+++ src/main/java/com/sofkaU/BibliotecaReactiva/BibliotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.BibliotecaReactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaReactivaApplication.class, args);
+	}
+
+}
